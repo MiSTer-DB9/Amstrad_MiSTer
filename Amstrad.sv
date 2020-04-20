1f255eeeac437aff50271e2702bbfb0ad4bb999b
Miguel-T80c <the80scomputers@gmail.com>
2020-04-20 12:01:46 +0200

    Fix release

--- Amstrad.sv
+++ Amstrad.sv
@@ -241,8 +241,8 @@ wire        forced_scandoubler;
 wire [21:0] gamma_bus;
 
 // S F2 F1 U D L R 
-wire [31:0] joy1 = joydb_1ena ? {joydb_1[10],joydb_1[6],joydb_1[5]|joydb_1[4],joydb_1[3:0]} : joy1_USB;
-wire [31:0] joy2 = joydb_2ena ? {joydb_2[10],joydb_2[6],joydb_2[5]|joydb_2[4],joydb_2[3:0]} : joydb_1ena ? joy1_USB : joy2_USB;
+wire [31:0] joy1 = joydb_1ena ? (OSD_STATUS? 32'b000000: {joydb_1[10],joydb_1[6],joydb_1[5]|joydb_1[4],joydb_1[3:0]}) : joy1_USB;
+wire [31:0] joy2 = joydb_2ena ? (OSD_STATUS? 32'b000000: {joydb_2[10],joydb_2[6],joydb_2[5]|joydb_2[4],joydb_2[3:0]}) : joydb_1ena ? joy1_USB : joy2_USB;
 
 wire [15:0] joydb_1 = JOY_FLAG[2] ? JOYDB9MD_1 : JOY_FLAG[1] ? JOYDB15_1 : '0;
 wire [15:0] joydb_2 = JOY_FLAG[2] ? JOYDB9MD_2 : JOY_FLAG[1] ? JOYDB15_2 : '0;
@@ -300,7 +300,7 @@ hps_io #(.STRLEN($size(CONF_STR)>>3), .VDNUM(2)) hps_io
 
 	.joystick_0(joy1_USB),
 	.joystick_1(joy2_USB),
-	.joy_raw(joydb_1[5:0]), //Menu Dirs, A:Action B:Back (OSD)
+	.joy_raw(OSD_STATUS? (joydb_1[5:0]|joydb_2[5:0]) : 6'b000000 ), //Menu Dirs, A:Action B:Back (OSD)
 
 	.buttons(buttons),
 	.status(status),
